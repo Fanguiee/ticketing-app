fa633047d20e79ef06b02a6971bc3dea7c9932b6
Fanguiee <duoraem@gmail.com>
2023-10-29 07:53:38 +0100

    create new ticket

---
+++ app/(models)/Ticket.js
@@ -0,0 +1,23 @@
+import mongoose, { Schema } from "mongoose";
+
+mongoose.connect(process.env.MongoDB_uri); //variable from the .env.local file
+mongoose.Promise = global.Promise;
+
+const ticketSchema = new Schema(
+  {
+    title: String,
+    description: String,
+    category: String,
+    priority: Number,
+    progress: Number,
+    status: String,
+    active: Boolean,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Ticket = mongoose.models.Ticket || mongoose.model("Ticket", ticketSchema);
+
+export default Ticket;
--- app/page.js
+++ app/page.js
@@ -1,10 +1,7 @@
 import React from "react";
-import { run } from "@/.env.local";
 
 const Home = () => {
   return <div>Home</div>;
 };
 
 export default Home;
-
-run().catch(console.dir);
