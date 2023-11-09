18fedfa2aec0c306e3797ff7762bb532f4c46e21
Fanguiee <duoraem@gmail.com>
2023-11-09 21:45:05 +0100

    final check of funtionalities

--- app/(components)/Nav.jsx
+++ app/(components)/Nav.jsx
@@ -7,14 +7,14 @@ const Nav = () => {
     <nav className="flex justify-between bg-nav p-4">
       <div className="flex items-center space-x-4">
         <Link href="/">
-          <FontAwesomeIcon icon={faHome} className="icon" />
+          <FontAwesomeIcon icon={faHome} className="icon text-default-icon" />
         </Link>
         <Link href="/TicketPage/new">
-          <FontAwesomeIcon icon={faTicket} className="icon" />
+          <FontAwesomeIcon icon={faTicket} className="icon text-default-icon" />
         </Link>
       </div>
       <div>
-        <p className=" text-default-text">duoraem@gmail.com</p>
+        <p className=" text-default-title">duoraem@gmail.com</p>
       </div>
     </nav>
   );
